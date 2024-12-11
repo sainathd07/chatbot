c189d38aecb8f89960b431b9ea4e0d410f7a65ae
sainathd07 <67309212+sainathd07@users.noreply.github.com>
2024-12-10 22:19:42 -0500

    Create payment.sol

---
+++ src/payment.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+contract Payment {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+}
